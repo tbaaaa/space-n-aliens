2cd8239ed66e7b705cc0580fc9d750baae5e4dde
tbaaaa <ayromulus8@gmail.com>
2026-01-01 14:14:54 -0400

    Adjust boss spawn threshold and update initial score for game balance

--- main.py
+++ main.py
@@ -53,9 +53,9 @@ enemy_bullets = []
 
 # Boss
 boss = None
-boss_spawn_threshold = 200
+boss_spawn_threshold = 300
 reflectable_projectiles = []
-score = 190
+score = 290
 
 # Final boss hazards
 swarm_minions = []  # kamikaze crash minions
@@ -96,8 +96,8 @@ def reset_game():
     active_lasers = []
     path_hazards = []
     explosion_effects = []
-    boss_spawn_threshold = 200
-    score = 190
+    boss_spawn_threshold = 300
+    score = 290
     frame_count = 0
     player_hp = 3
     invincible = False
